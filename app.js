85dc33bc4146e388c0e71282ad435b32ea25d3a4
Gregory S. Kielian <gregory.kielian@gmail.com>
2016-07-17 20:23:22 -0700

    adding primitive operation examples

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
 var firebase = require("firebase");
+
 firebase.initializeApp({
-    serviceAccount: "path/to/serviceAccountCredentials.json",
-    databaseURL: "https://databaseName.firebaseio.com"
+  databaseURL: "https://crackling-heat-8070.firebaseio.com"
 });
+
+//example of getting data once
+var db = firebase.database();
+var ref = db.ref("/public");
+ref.once("value", function(snapshot) {
+  console.log(snapshot.val());
+});
+
+
+//example of setting data
+var usersRef = ref.child("users");
+usersRef.set({
+  alanisawesome: {
+    date_of_birth: "June 23, 1912",
+    full_name: "Alan Turing"
+  },
+  gracehop: {
+    date_of_birth: "December 9, 1906",
+    full_name: "Grace Hopper"
+  }
+});
+
+
+//example of updating saved data
+var hopperRef = usersRef.child("gracehop");
+hopperRef.update({
+  "nickname": "Amazing Grace"
+});
+
+
+
